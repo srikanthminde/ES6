b6948dca1eff90c4544eb763d76e0d8cf0a26336
srikanth7176 <srikanth9705377176@gmail.com>
2024-09-09 15:16:52 +0530

    ES6 Assignments

---
+++ rust.varabl/main.js
@@ -0,0 +1,3 @@
+import  mydef,{add,sub} from './utils.js';
+console.log(add(10,40))
+console.log( mydef())
---
+++ rust.varabl/utils.js
@@ -0,0 +1,18 @@
+export const add = function(a,b){
+    return a+b
+}
+
+export const sub = function(a,b){
+    return a-b
+}
+
+export const mult = function(a,b){
+    return a*b
+}
+
+export const div = function(a,b){
+    return a/b
+}
+export default function xyz(){
+    return "srikanth"
+}
